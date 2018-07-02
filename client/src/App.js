35e4f087b4465f0954f8806604e7330791630aaf
Charles <charlescollings@usa.net>
2018-07-01 22:29:08 -0500

    randomized_gamePiece_functionality

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
 import "./App.css";
+import Instructions from "./components/Instructions/Instructions";
 
 var score = 0;
 var chosen = [];
@@ -27,18 +28,23 @@ class App extends Component {
       chosen = []
       // show message that game is over and click any image to start over
       // rerender imagages randomly
-
+      const friends = this.state.friends.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
+      
+      this.setState({ friends });
       this.setState({ score });
       this.setState({ chosen });
-    };
-      // else add id to chosen array
-      chosen.push(id)
-      // add 1 to score
-      score++
-      // rerender imagages randomly
-
-      this.setState({ score });
-      this.setState({ chosen });
+    };  {
+        // else add id to chosen array
+        chosen.push(id)
+        // add 1 to score
+        score++
+        // rerender imagages randomly
+        const friends = this.state.friends.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
+        
+        this.setState({ friends });
+        this.setState({ score });
+        this.setState({ chosen });
+        }
   }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
@@ -46,6 +52,7 @@ class App extends Component {
     return (
       <Wrapper>
         <Title>Memory Game</Title>
+        <Instructions>Click on an image to earn points, but don't click on any more than once!</Instructions>
         <ScorePanel       
           score={score}
         />
---
+++ client/src/components/Instructions/Instructions.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Instructions.css";
+
+const Instructions = props => <p className="instructions">{props.children}</p>;
+
+export default Instructions;
